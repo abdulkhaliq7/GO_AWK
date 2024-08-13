8f9fdc018269716184607717c3c807ddc96f61eb
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-08-13 09:40:08 +0300

    added cpu checker

--- main.go
+++ main.go
@@ -3,12 +3,28 @@ package main
 import (
 	"fmt"
 	"github/abdulkhaliq/awk/awk"
+	"log"
+	"os"
+	"runtime/pprof"
 )
 
 func main() {
 
+	// Start CPU profiling
+	f, err := os.Create("cpu.pprof")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	if err := pprof.StartCPUProfile(f); err != nil {
+		log.Fatal(err)
+	}
+	defer pprof.StopCPUProfile()
+
+	//files read
 	data := `633022241|4000000|$30|20240731
-633022241|4000000|$30|20240731`
+	633022241|4000000|$30|20240731`
 
 	newAwk := awk.NewAwk(data)
 
