2459767165286c8631bb730db667ac83d8dd70c2
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-08-15 10:54:19 +0300

    added filter function to simplify the awk function

--- awk/awk.go
+++ awk/awk.go
@@ -33,49 +33,53 @@ func (a *Awk) DataSplit(splitField, printingField string, chosenColumns ...strin
 
 		line := scanner.Text()
 
-		if len(chosenColumns) == 0 {
-
-			splittedData := strings.Split(line, splitField)
-
-			for _, all := range splittedData {
-				fieldsChosen += fmt.Sprintf("%v%v", all, printingField)
-			}
-
-			newLine := "\n"
-
-			fieldsChosen += fmt.Sprintf("%v", newLine)
-
-		} else {
-			for _, value := range chosenColumns {
-
-				c := value
-
-				column, err := strconv.Atoi(c)
-
-				if err != nil {
-
-					log.Printf("the string Converter did not work : %v", err)
-				}
-
-				splittedData := strings.Split(line, splitField)
-
-				if len(splittedData) > column {
-					fieldsChosen += fmt.Sprintf("%v%v", splittedData[column], printingField)
-				} else {
-
-					log.Printf("the chosen column %v is greater than the length of the string %v", column, len(splittedData))
-				}
-
-			}
-
-			newLine := "\n"
-
-			fieldsChosen += fmt.Sprintf("%v", newLine)
-		}
+		filter(line, splitField, printingField, &fieldsChosen, chosenColumns...)
 
 	}
 
 	return &Awk{Data: fieldsChosen}
+}
+
+func filter(line, splitField, printingField string, fieldsChosen *string, chosenColumns ...string) {
+	if len(chosenColumns) == 0 {
+
+		splittedData := strings.Split(line, splitField)
+
+		for _, all := range splittedData {
+			*fieldsChosen += fmt.Sprintf("%v%v", all, printingField)
+		}
+
+		newLine := "\n"
+
+		*fieldsChosen += fmt.Sprintf("%v", newLine)
+
+	} else {
+		for _, value := range chosenColumns {
+
+			c := value
+
+			column, err := strconv.Atoi(c)
+
+			if err != nil {
+
+				log.Printf("the string Converter did not work : %v", err)
+			}
+
+			splittedData := strings.Split(line, splitField)
+
+			if len(splittedData) > column {
+				*fieldsChosen += fmt.Sprintf("%v%v", splittedData[column], printingField)
+			} else {
+
+				log.Printf("the chosen column %v is greater than the length of the string %v", column, len(splittedData))
+			}
+
+		}
+
+		newLine := "\n"
+
+		*fieldsChosen += fmt.Sprintf("%v", newLine)
+	}
 }
 
 func (a *Awk) Replace(old, new string) *Awk {
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github/abdulkhaliq/awk/awk"
 	"log"
 	"os"
-	"runtime/pprof"
 	"time"
 )
 
@@ -15,48 +14,26 @@ func main() {
 	// Get the current time
 	start := time.Now()
 
-	// Start CPU profiling
-	f, err := os.Create("cpu.pprof")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	if err := pprof.StartCPUProfile(f); err != nil {
-		log.Fatal(err)
-	}
-	defer pprof.StopCPUProfile()
-
 	file, err := os.Open("cdr.txt")
 
 	if err != nil {
 		log.Println(err)
 	}
 
-	// Create a buffer to read the file
-	bufferSize := 10485760
-	// Read strings using bufio.Reader
-	for {
-
-		reader := bufio.NewReaderSize(file, bufferSize)
-
-		// Read until newline and store in a buffer
-		line, err := reader.ReadString('\n')
-
-		fmt.Println(line)
-		if err != nil {
-			if err.Error() != "EOF" {
-				log.Fatal(err)
-			}
-		}
-
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// Print each line
+		line := scanner.Text()
 		newAwk := awk.NewAwk(line)
 
 		output := newAwk.DataSplit("|", "", "35").DataSplit(`\`, "", "26", "70")
 
 		fmt.Println(output.Data)
 	}
-
 	fmt.Println("Total time taken to complete The whole process : ", time.Since(start))
 
 }
+
+
+
+
