41f2bdb27d4876a80517f334d5b7bbfa42462350
abdulkhaliq7 <abdikhaliq_7@hotmail.com>
2024-08-18 09:47:01 +0300

    removed go routine and reduced the processing time 1 minute

--- awk/awk.go
+++ awk/awk.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 type Awk struct {
@@ -20,98 +19,43 @@ func NewAwk(data string) *Awk {
 
 func (a *Awk) DataSplit(splitField, printingField string, chosenColumns ...string) *Awk {
 
-	var wg sync.WaitGroup
-
-	channel := make(chan string, 200)
-	// Create a new reader for the data string
 	reader := strings.NewReader(a.Data)
-
-	// Create a scanner to read data line by line
 	scanner := bufio.NewScanner(reader)
-
-	// Scan the data line by line
-
-	//fmt.Println(a.Data)
-
-	for scanner.Scan() {
-		wg.Add(1)
-
-		line := scanner.Text()
-
-		go filter(line, splitField, printingField, channel, &wg, chosenColumns...)
-
-	}
-
-	go func() {
-		wg.Wait()
-		close(channel)
-	}()
-
-	for output := range channel {
-		return &Awk{Data: output}
-	}
-
-	return nil
-}
-
-func filter(line, splitField, printingField string, channel chan<- string, wg *sync.WaitGroup, chosenColumns ...string) {
-
-	//var fieldsChosen string
-
 	var fieldsChosen strings.Builder
-
-	defer wg.Done()
-
-	if len(chosenColumns) == 0 {
-
-		splittedData := strings.Split(line, splitField)
-
-		for _, all := range splittedData {
-			//fieldsChosen += fmt.Sprintf("%v%v", all, printingField)
-			fieldsChosen.WriteString(all + printingField)
-		}
-
-		newLine := "\n"
-
-		//fieldsChosen += fmt.Sprintf("%v", newLine)
-		fieldsChosen.WriteString(newLine)
-
-	} else {
-		for _, value := range chosenColumns {
-
-			c := value
-
-			column, err := strconv.Atoi(c)
-
-			if err != nil {
-
-				log.Printf("the string Converter did not work : %v", err)
-			}
-
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(chosenColumns) == 0 {
 			splittedData := strings.Split(line, splitField)
-
-			if len(splittedData) > column {
-				//fieldsChosen += fmt.Sprintf("%v%v", splittedData[column], printingField)
-				fieldsChosen.WriteString(splittedData[column] + printingField)
-			} else {
-
-				log.Printf("the chosen column %v is greater than the length of the string %v", column, len(splittedData))
+			for _, all := range splittedData {
+				fieldsChosen.WriteString(all)
+				fieldsChosen.WriteString(printingField)
 			}
-
+			newLine := "\n"
+			fieldsChosen.WriteString(newLine)
+		} else {
+			for _, value := range chosenColumns {
+				c := value
+				column, err := strconv.Atoi(c)
+				if err != nil {
+					log.Printf("the string Converter did not work : %v", err)
+				}
+				splittedData := strings.Split(line, splitField)
+				if len(splittedData) > column {
+					fieldsChosen.WriteString(splittedData[column])
+					fieldsChosen.WriteString(printingField)
+				} else {
+					log.Printf("the chosen column %v is greater than the length of the string %v", column, len(splittedData))
+				}
+			}
+			newLine := "\n"
+			fieldsChosen.WriteString(newLine)
 		}
-
-		newLine := "\n"
-
-		//fieldsChosen += fmt.Sprintf("%v", newLine)
-		fieldsChosen.WriteString(newLine)
 	}
-
-	channel <- fieldsChosen.String()
+	return &Awk{Data: fieldsChosen.String()}
 }
 
 func (a *Awk) Replace(old, new string) *Awk {
 
 	updatedData := strings.ReplaceAll(a.Data, old, new)
-
 	return &Awk{Data: updatedData}
 }
